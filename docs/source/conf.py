4e6efe0fe2df6ec4bb3adfad4eec542c0d61e59b
installhpprintersetup <lora63527@gmail.com>
2025-01-25 13:41:56 +0530

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,6 +7,7 @@ import os
 import sys
 
 # -- Path setup --------------------------------------------------------------
+
 # If extensions or modules to document with autodoc are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
@@ -21,21 +22,26 @@ author = 'Setup HP printer'
 # The full version, including alpha/beta/rc tags
 release = '1.0.0'
 
-# -- Google site verification ------------------------------------------------
+
+
+# Inject Google verification meta tag into the HTML head
 def add_google_verification(app, pagename, templatename, context, doctree):
     context['google_verification'] = '''
     <meta name="google-site-verification" content="fL4pdqYIa6vMjXlU0Q6XLuzxD2ZBar5O7O6aaVIQI4Q" />
     '''
     return context
 
-# -- Favicon ---------------------------------------------------------------
-html_favicon = 'favicon.ico'  # Make sure your favicon.ico file is in the static directory
+
+
+
+# Favicon
+html_favicon = 'favicon.ico'
 
 # -- General configuration ---------------------------------------------------
 
-# Add any Sphinx extension module names here, as strings.
+# Add any Sphinx extension module names here, as strings. 
 # These extensions are optional but useful for Read the Docs.
-# extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx.ext.viewcode']
+
 
 # Add any paths that contain templates here, relative to this directory.
 # templates_path = ['_templates']
@@ -49,24 +55,15 @@ html_favicon = 'favicon.ico'  # Make sure your favicon.ico file is in the static
 
 # The theme to use for HTML and HTML Help pages.
 # Use the Read the Docs theme for better compatibility with Read the Docs.
-html_theme = 'alabaster'
-html_theme = 'sphinx_rtd_theme' 
+# html_theme = 'sphinx_rtd_theme'
+
 # Add any paths that contain custom static files (such as style sheets)
 # here, relative to this directory. These files are copied after the built-in
 # static files, so a file named "default.css" will overwrite the built-in one.
-html_static_path = ['_static']
+# html_static_path = ['_static']
 
-# -- Options for HTML output -------------------------------------------------
-
-# Hide "View page source" link and copyright
-html_show_sourcelink = False
+html_show_sourcelink = False  # Hides the "View page source" link
 html_show_copyright = False
-
-# Customize the theme options
 html_theme_options = {
     'show_powered_by': False,  # Disable "Powered by Sphinx"
 }
-
-# Hook to add the Google verification meta tag to every page
-def setup(app):
-    app.connect('html-page-context', add_google_verification)
